0c8324dfc6b236b4446b3eb6ee726d430d028913
Sebastián <>
2021-10-07 18:35:49 +0100

    Kata2 version 5

--- Documents/NetBeansProjects/Kata2/src/kata2/Kata2.java
+++ Documents/NetBeansProjects/Kata2/src/kata2/Kata2.java
@@ -1,6 +1,5 @@
 package kata2;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class Kata2 {
@@ -10,7 +9,6 @@ public class Kata2 {
         
        String[] data = {"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"};
 
-       
         Histogram histo = new Histogram(data);
         
         Map<String, Integer> histogr = histo.getHistogram();
@@ -26,92 +24,3 @@ public class Kata2 {
     }
 }
     
-
-
-
-
-
-
-      //Versión 1
-      /*  int[] data = {1,2,2,3,3,4,5,1,2,1,2,1,5,6,4};
-        
-        Map<Integer, Integer> histogram = new HashMap<>();
-        
-        for (int i = 0; i < data.length; i++) {
-            if(histogram.containsKey(data[i])){
-                histogram.put(data[i], histogram.get(data[i])+1);
-            }
-            else
-                histogram.put(data[i], 1);
-        }
-    
-        System.out.println("---Mostramos el resultado---");
-        
-        if(!histogram.isEmpty()){
-            for(Map.Entry<Integer, Integer> entry : histogram.entrySet()) { 
-                System.out.println(entry.getKey() + " ==> " + entry.getValue());
-            }
-        }   
-
-
-//====================
-        //Version 2
-        
-       int[] data = {1,2,2,3,3,4,5,1,2,1,2,1,5,6,4};
-        
-       Map<Integer, Integer> histogram = new HashMap<>();
-       
-        for (int key : data) {
-            histogram.put(key, histogram.containsKey(key) ? histogram.get(key)+1 : 1);   
-        } 
-        System.out.println("---Mostramos el resultado---");
-        
-        if(!histogram.isEmpty()){
-            for(Map.Entry<Integer, Integer> entry : histogram.entrySet()) { 
-                System.out.println(entry.getKey() + " ==> " + entry.getValue());
-            }
-        }
-    }
-
-
-//====================
-        //Version 3
-        
-       int[] data = {1,2,2,3,3,4,5,1,2,1,2,1,5,6,4};
-       
-        Histogram histo = new Histogram(data);
-        
-        Map<Integer, Integer> histogr = histo.getHistogram();
-        
-        
-        System.out.println("---Mostramos el resultado---");
-        
-        if(!histogr.isEmpty()){
-            for(Map.Entry<Integer, Integer> entry : histogr.entrySet()) { 
-                System.out.println(entry.getKey() + " ==> " + entry.getValue());
-            }
-        }
-
-
-//==================
-
-        
-        //Version 4
-        
-       Integer[] data = {1,2,2,3,3,4,5,1,2,1,2,1,5,6,4};
-       
-        Histogram histo = new Histogram(data);
-        
-        Map<Integer, Integer> histogr = histo.getHistogram();
-        
-        
-        System.out.println("---Mostramos el resultado---");
-        
-        if(!histogr.isEmpty()){
-            for(Map.Entry<Integer, Integer> entry : histogr.entrySet()) { 
-                System.out.println(entry.getKey() + " ==> " + entry.getValue());
-            }
-        }
-        */
-        
-
